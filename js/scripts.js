ca03298689ced8e772efc49f8ebaf3d22a77cf54
EllyKiplaa <ellykiplangat200@gmail.com>
2020-05-17 22:30:05 +0300

    Adding up the inputted details so that it can show up basically at the size crust toppings and quality

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+const add = document.getElementById("add");
+
+add.addEventListener("click", function() {
+    const size = parseInt(document.getElementById("sizes").value);
+    const crust = parseInt(document.getElementById("crusts").value);
+    const topping = parseInt(document.getElementById("toppings").value);
+    const quantity = parseInt(document.getElementById("quantity").value);
+
+});
