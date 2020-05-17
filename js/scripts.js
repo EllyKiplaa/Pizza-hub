bf3ba12f0cf464b671c405cb92749132adbc478f
EllyKiplaa <ellykiplangat200@gmail.com>
2020-05-17 22:42:34 +0300

    added another lines of code containing prototypes and methods

--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,24 @@ add.addEventListener("click", function() {
     const results = function() {
         const table = document.getElementById("table");
         const row = table.insertRow();
-       
+        const quantity1 = row.insertCell();
+        const size2 = row.insertCell();
+        const crust4 = row.insertCell();
+        const toppings3 = row.insertCell();
+        const total5 = row.insertCell();
+        quantity1.innerHTML = quantity;
+        size2.innerHTML = size;
+        crust4.innerHTML = crust;
+        toppings3.innerHTML = topping;
+        total5.innerHTML = (size + crust + topping) * quantity;
+        size2.innerHTML = size;
+        crust4.innerHTML = crust.options[crust.selectedIndex].text;
+        toppings3.innerHTML = topping;
+        quantity1.innerHTML = quantity.options[quantity.selectedIndex].text;
 
     }
     results();
 })
+
+
+
